95cc21005bb99a77d1b6bb71cc2e081e594f2e43
Ruslan <webronik@gmail.com>
2018-05-02 23:32:13 +0500

    Оптимизирован ход компьютера, доработан возврат свободной ячейки выбранной рандомно.

--- src/pages/single-player/single-player.ts
+++ src/pages/single-player/single-player.ts
@@ -8,6 +8,7 @@ import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angula
 })
 export class SinglePlayerPage {
 
+
   board: number[];
 
   boardRows: number[] = [1, 2, 3];
@@ -27,6 +28,7 @@ export class SinglePlayerPage {
   firstGamerNumber: number = 1;
   secondGamerNumber: number = 2;
   gamer: string;
+  finishGame: FinishGame;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
@@ -58,57 +60,6 @@ export class SinglePlayerPage {
     this.setGamer(this.firstGamer);
   }
 
-  /**
-   * Поиск оптимального хода
-   *
-   * @returns {number} - номер ячейки для хода
-   */
-  searchMove(): number {
-    let cell: number = 0;
-    let estimateMoveOpponent: number; // оценка хода соперника
-    let estimateMove: number; // оценка своего хода
-    for (let lc of this.winCombination) {
-      estimateMoveOpponent = 0;
-      estimateMove = 0;
-      //console.log(lc)
-      // цикл для поиска предвыиграшной комбинации - "занято две из трех ячеек"
-      for (let pos of lc) {
-        let boardPosVal = this.board[pos - 1];
-        if (boardPosVal == this.firstGamerNumber) {
-          estimateMoveOpponent += boardPosVal;
-        } else if (boardPosVal == this.secondGamerNumber) {
-          estimateMove += boardPosVal;
-        }
-      }
-
-      //console.log("estimateMoveOpponent = "+estimateMoveOpponent+"; estimateMove = "+estimateMove)
-
-      // если у оппонента следующий ход может быть выигрышным (две из трех ячеек по выиграшной линии отмечены), то займем свободную сами
-      if (estimateMoveOpponent > this.firstGamerNumber) {
-        for (let pos of lc) {
-          if (this.board[pos - 1] == 0) {
-            cell = pos;
-          }
-        }
-        break;
-      } else if (estimateMove > this.secondGamerNumber) {
-        for (let pos of lc) {
-          if (this.board[pos - 1] == 0) {
-            cell = pos;
-          }
-        }
-        break;
-      }
-    }
-
-    // если нигде не нашли предвыиграшную комбинацию, то сходим рандомно
-    if(cell == 0) {
-      cell = this.freeCellGameBoard()
-    }
-
-    return cell;
-  }
-
   // закрыть страницу
   goBack() {
     this.navCtrl.pop();
@@ -116,9 +67,12 @@ export class SinglePlayerPage {
 
 
   doConfirm(gamerSymbol: string) {
+    let msgWin: string, msgDraw: string;
+    msgWin = gamerSymbol == "1" ? 'Молодец, ты победил!' : 'Ну ты лузер, ты проиграл!';
+    msgDraw = 'Ничья!!!';
     let confirm = this.alertCtrl.create({
       title: 'УРАААА!!!',
-      message: 'Победил   игрок ' + gamerSymbol,
+      message: this.finishGame.finishType == 'WIN' ? msgWin : msgDraw, // TODO: enum
       buttons: [
         {
           text: 'Уходим отсюда',
@@ -139,11 +93,11 @@ export class SinglePlayerPage {
 
 
   goPlay(cell: number) {
-    console.log('игрок ' + this.firstGamer + ': id_' + cell);
+    //console.log('игрок ' + this.firstGamer + ': id_' + cell);
     this.play(this.firstGamer, cell);
     if (this.getGamer() == this.secondGamer) {
       let cellBoard = this.searchMove(); //this.freeCellGameBoard();
-      console.log('игрок ' + this.secondGamer + ': id_' + cellBoard);
+      //console.log('игрок ' + this.secondGamer + ': id_' + cellBoard);
       this.play(this.secondGamer, cellBoard);
     }
   }
@@ -155,85 +109,138 @@ export class SinglePlayerPage {
       this.setSymbolOnGameBoard(gamerNumber, cell);
       let className: string = this.getGamerClassName(gamer);
       // на какую ячейку ходит
-      console.log("id_" + cell.toString())
       document.getElementById("id_" + cell.toString()).classList.add(className);
-      // проверим, вдруг выиграл?
-      if (this.isWinning(gamer)) {
-        console.log('WIN!!!!!!!!!');
+      // проверим, выигрышный ход или ничья
+      this.finishGame = this.checkFinishGame(gamer);
+      if (this.finishGame.isFinish) {
         this.doConfirm(gamerNumber.toString());
-      } else { //TODO: дописать ничью..
+      } else {
         // передать ход другому
         this.setNextGamer();
+
       }
     } else {
+      // TODO: доработать кейс, когда выбирают занятую чяейку
       console.log('ячейка занята');
     }
-    //this.gameBoardPrint();
     this.boardPrint();
   }
 
+  /**
+   * Поиск оптимального хода
+   *
+   * @returns {number} - номер ячейки для хода
+   */
+  searchMove(): number {
+    let cell: number = 0;
+    let estimateMoveOpponent: number; // оценка хода соперника
+    let estimateMove: number; // оценка своего хода
+    for (let lc of this.winCombination) {
+      estimateMoveOpponent = 0;
+      estimateMove = 0;
+      // цикл для поиска предвыиграшной комбинации - "занято две из трех ячеек"
+      for (let pos of lc) {
+        let boardPosVal = this.board[pos - 1];
+        if (boardPosVal == this.firstGamerNumber) {
+          estimateMoveOpponent += boardPosVal;
+        } else if (boardPosVal == this.secondGamerNumber) {
+          estimateMove += boardPosVal;
+        }
+      }
+
+      // TODO: дописать ход на выигрыш, если два-два..
+      // если у оппонента следующий ход может быть выигрышным (две из трех ячеек по выиграшной линии отмечены), то займем свободную сами
+      if (estimateMoveOpponent > this.firstGamerNumber && estimateMove != this.secondGamerNumber) {
+        for (let pos of lc) {
+          if (this.board[pos - 1] == 0) {
+            cell = pos;
+          }
+        }
+        break;
+      } else if (estimateMove > this.secondGamerNumber) {
+        for (let pos of lc) {
+          if (this.board[pos - 1] == 0) {
+            cell = pos;
+          }
+        }
+        break;
+      }
+    }
+
+    // если нигде не нашли предвыиграшную комбинацию, то сходим рандомно
+    if(cell == 0) {
+      cell = this.getFreeCellRandom()
+    }
+
+    return cell;
+  }
 
   /**
-   *  Свободная ячейка
+   *  Свободная ячейка рандомно
    *
    * @returns {number}
    */
-  freeCellGameBoard(): number {
-    /*
-    let randomCell: number = this.randomInt(1, 9);
-
-    console.log("randomCell = "+randomCell)
-
-
-    if (this.checkCellIsEmpty(randomCell)){
-      return randomCell;
-    } else {
-      this.checkCellIsEmpty(randomCell);
+  getFreeCellRandom(): number {
+    let randomCell: number;
+    let cellValue: number = 1;
+    while (cellValue != 0){
+      randomCell = this.getRandomInt(1, 9);
+      cellValue = this.board[randomCell - 1];
     }
-    */
-    for (let i = 0; i < this.board.length; i++) {
-      // TODO: доработать, сейчас возвращает первую свободную
-      if (this.board[i] == 0) {
-        return ++i;
-      }
-    }
+    return randomCell;
   }
 
-  getRandomCell(){
-
-  }
-
-  randomInt(min, max){
+  /**
+   * Подучить рандомно целое число в диапазоне от min до max
+   *
+   * @param min
+   * @param max
+   * @returns {any}
+   */
+  getRandomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-
   /**
-   *  Проверим выигрышной комбинации
+   *  Проверить на выигрышную комбинацию или на ничью
    *
    * @param {string} gamer
    * @returns {boolean}
    */
-  isWinning(gamer: string): boolean {
+  checkFinishGame(gamer: string): FinishGame {
     let gamerNumber = this.getGamerNumber(gamer);
     let count: number;
 
+    // проверка на выиграш одного из участников
     for (let lc of this.winCombination) {
       count = 0;
       for (let pos of lc) {
-        let boardPosVal = this.board[pos - 1];
-        if (boardPosVal == gamerNumber) {
+        let boardVal = this.board[pos - 1];
+        if (boardVal == gamerNumber) {
           count++;
         }
         if (count == 3) {
-          return true;
+          return new FinishGame(true, "WIN"); // TODO: Переделать на enum FinishGame
         }
       }
     }
 
+    // проверка на ничью
+    let isCheckFreeCell: boolean = false;
+    for (let boardVal of this.board) {
+      if (boardVal == 0) {
+        isCheckFreeCell = true;
+      }
+    }
+
+    if (!isCheckFreeCell) {
+      return new FinishGame(true, "DRAW"); // TODO: Переделать на enum FinishGame
+    }
+
     //console.log("count = "+count)
-    return false;
+    return new FinishGame(false, null);
   }
+
 
   setSymbolOnGameBoard(gamerSymbol: number, cell: number) {
     this.board[cell - 1] = gamerSymbol;
@@ -255,21 +262,15 @@ export class SinglePlayerPage {
     return gamer == this.firstGamer ? 'cell-background1' : 'cell-background2';
   }
 
-  /*getGamerSymbol(gamer: string): string {
-    return gamer == this.firstGamer ? this.firstGamerSymbol : this.secondGamerSymbol;
-  }*/
+
   getGamerNumber(gamer: string): number {
     return gamer == this.firstGamer ? this.firstGamerNumber : this.secondGamerNumber;
   }
 
   checkCellIsEmpty(cell: number): boolean {
-    //return this.gameBoard[row - 1][col - 1] == null;
     return this.board[cell - 1] == 0;
   }
 
-  /*getCellId(index: string): string {
-    return 'id_' + index;
-  }*/
 
   getCellId(row: number, col: number): string {
     return "id_" + this.getCellIndex(row, col).toString();
@@ -308,4 +309,14 @@ export class SinglePlayerPage {
     console.log(sb);
   }
 
+}
+
+class FinishGame {
+  isFinish: boolean;
+  finishType: string;
+
+  constructor(isFinish: boolean, finishType: string) {
+    this.isFinish = isFinish;
+    this.finishType = finishType;
+  }
 }
